d92fdc3de1a7865d2ff1e084fcedaa041d42075a
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-10-04 17:16:36 -0500

    some styles, text issue

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -3,8 +3,10 @@ import React from "react"
 export default function AnimeCard({ anime }) {
     return (
         <div className="anime-card">
+            <a href={anime.url}>
             <img src={anime.image} className="anime-image"/>
-            <h4>{anime.title}</h4>
+            </a>
+            <h4 className="anime-title">{anime.title}</h4>
         </div>
     )
 }
