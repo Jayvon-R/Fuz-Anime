ce0e183cd85c7b5f34bf2b96d6bd042a9b4c3973
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-10-24 12:52:33 -0500

    code cleanup

--- src/components/AnimeInfo.js
+++ src/components/AnimeInfo.js
@@ -1,49 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import AddRatingForm from "./AddRatingForm";
 import { scrapeReviewsForAnime } from "./webScraper";
 
 export default function AnimeInfo() {
   const { id } = useParams();
   const [animeData, setAnimeData] = useState(null);
-  const [ratings, setRatings] = useState([]);
-  const [userRating, setUserRating] = useState();
-  const [reviews, setReviews] = useState([]);
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        `http://localhost:3001/animeRatings?animeId=${id}`
-      );
-      setRatings(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  useEffect(() => {
+    // const scrapeAnimeReviews = async () => {
+    //   const reviewsData = await scrapeReviewsForAnime(animeData.title, 5);
+    //   setReviews(reviewsData);
+    // };
 
-  const scrapeAnimeReviews = async () => {
-    const reviewsData = await scrapeReviewsForAnime(animeData.title, 5);
-    setReviews(reviewsData);
-  };
+    const fetchAnimeData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.consumet.org/anime/gogoanime/info/${id}`
+        );
+        setAnimeData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  const fetchAnimeData = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.consumet.org/anime/gogoanime/info/${id}`
-      );
-      setAnimeData(response.data);
-      fetchData();
-      scrapeAnimeReviews();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  fetchAnimeData();
-}, [id, animeData.title]);
-
+    fetchAnimeData();
+  }, [id]);
 
   return (
     <div>
@@ -66,18 +48,7 @@ useEffect(() => {
             </p>
             <p className="sub-or-dub">Sub or Dub: {animeData.subOrDub}</p>
             <p className="type">Type: {animeData.type}</p>
-            <AddRatingForm
-              animeData={animeData}
-              userRating={userRating}
-              setUserRating={setUserRating}
-            />
           </div>
-
-          <ul>
-            {ratings.map((rating) => (
-              <li key={rating.id}>Rating: {rating.rating}</li>
-            ))}
-          </ul>
 
           <ul className="episode-list">
             {animeData.episodes.map((episode) => (
@@ -85,17 +56,6 @@ useEffect(() => {
                 <a href={episode.url} target="" className="episode-link">
                   EP {episode.number}
                 </a>
-              </li>
-            ))}
-          </ul>
-
-          <ul className="reviews-list">
-            {reviews.map((review, index) => (
-              <li key={index}>
-                <h4>Review {index + 1}</h4>
-                <p>Reviewer: {review.reviewer}</p>
-                <p>Score: {review.score}</p>
-                <p>Content: {review.content}</p>
               </li>
             ))}
           </ul>
